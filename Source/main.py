dfee5951995295130de77dfcb8b07852da534866
Gia Duoc Quach <quachgiaduoc2004@gmail.com>
2025-06-18 22:23:24 +0700

    update police move

--- Source/main.py
+++ Source/main.py
@@ -2,10 +2,12 @@ import sys
 import pygame
 import random
 
+from Algorithms.Police_Move import move_police_using_astar
 from Algorithms.SearchAlgorithms import SearchAlgorithms
 from Object.Food import Food
 from Object.Player import Player
 from Object.Wall import Wall
+from Extension.extension import DDX, Police_check, Thief_check
 from constants import *
 from Object.Menu import Menu, Button
 
@@ -111,4 +113,68 @@ class GameManager:
 
         text_surface = self.game_font.render(f'Score: {self.state.score}', False, RED)
         self.screen.blit(text_surface, (0, 0))
+
+    def generate_police_movements(self, movement_type=0):
+        new_police_positions = []
         
+        if movement_type == 1:
+            for idx, police in enumerate(self.state.police_objects):
+                [row, col] = police.getRC()
+
+                direction = random.randint(0, 3)
+                new_row, new_col = row + DDX[direction][0], col + DDX[direction][1]
+                while not Police_check(self.state.maze_map, new_row, new_col, self.state.height, self.state.width):
+                    direction = random.randint(0, 3)
+                    new_row, new_col = row + DDX[direction][0], col + DDX[direction][1]
+
+                new_police_positions.append([new_row, new_col])
+
+        elif movement_type == 2:
+            for police in self.state.police_objects:
+                [police_row, police_col] = police.getRC()
+                [thief_row, thief_col] = self.state.thief_player.getRC()
+                new_pos = move_police_using_astar(
+                    self.state.maze_map, police_row, police_col, 
+                    thief_row, thief_col, self.state.height, self.state.width
+                )
+                new_police_positions.append(new_pos)
+
+        return new_police_positions
+        
+    def check_collision_with_police(self, thief_row=-1, thief_col=-1):
+        thief_position = [thief_row, thief_col]
+        if thief_row == -1:
+            thief_position = self.state.thief_player.getRC()
+            
+        for police in self.state.police_objects:
+            police_position = police.getRC()
+            if thief_position == police_position:
+                return True
+
+        return False
+
+    def process_police_movement(self, police_new_positions, movement_timer):
+        for idx in range(len(self.state.police_objects)):
+            [old_police_row, old_police_col] = self.state.police_objects[idx].getRC()
+            [new_police_row, new_police_col] = police_new_positions[idx]
+
+            if old_police_row < new_police_row:
+                self.state.police_objects[idx].move(1, 0)
+            elif old_police_row > new_police_row:
+                self.state.police_objects[idx].move(-1, 0)
+            elif old_police_col < new_police_col:
+                self.state.police_objects[idx].move(0, 1)
+            elif old_police_col > new_police_col:
+                self.state.police_objects[idx].move(0, -1)
+
+            if movement_timer >= SIZE_WALL: 
+                self.state.police_objects[idx].setRC(new_police_row, new_police_col)
+
+                self.state.maze_map[old_police_row][old_police_col] = EMPTY
+                self.state.maze_map[new_police_row][new_police_col] = POLICE
+
+                for index in range(len(self.state.food_objects)):
+                    [food_row, food_col] = self.state.food_objects[index].getRC()
+                    if food_row == old_police_row and food_col == old_police_col:
+                        self.state.maze_map[food_row][food_col] = FOOD
+                        break        
